6f767986d4d696545b0e809dcc717c6e037d7d29
nurnaharmoly <you@example.com>
2018-08-28 13:13:33 +0600

    vari

---
+++ New folder/chap2/src/chap2/VariableEx.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chap2;
+
+/**
+ *
+ * @author User
+ */
+public class VariableEx {
+    public class VariableEx{
+ //int x;
+ //int y;
+ //int z;
+ 
+ //int x,y,z;
+ 
+ 
+ int x=1;
+ int y=1;
+ int z=1;
+ 
+ int x=y=z;
+ 
+ int x=1;
+x=5;
+
+int y=x;
+int z=x+y;
+
+
+final double PI = 3.14;
+ 
+    
+}
